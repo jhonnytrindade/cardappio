0ced718e82970754f35a9e247fece4af8d54d5ae
Jhonny Lopes <jhonny.lopes@discover.com.br>
2021-05-31 22:48:00 -0300

    retirada dependencias webpack.common

--- webpack.common.js
+++ webpack.common.js
@@ -55,11 +55,5 @@ module.exports = {
         flatten: true
       }
     ])
-  ],
-
-  externals: {
-    // require("jquery") is external and available
-    //  on the global var jQuery
-    "jquery": "jQuery"
-  }
+  ]
 };
