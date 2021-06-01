369bbd7d9dcba4ba4e88eccc71dada4f06b01c06
Jhonny Lopes <jhonny.lopes@discover.com.br>
2021-05-31 22:42:56 -0300

    scripts de volta ao static

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 // JS Goes here - ES6 supported
 
-import "./bootstrap.min.js";
-import "./venobox.min.js";
-import "./wow.min.js";
-import "./slick.min.js";
-import "./principal.js";
-
 import "./css/animate.min.css";
 import "./css/bootstrap.min.css";
 import "./css/font-awesome.min.css";
