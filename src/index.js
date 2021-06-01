f1c7ee841cd3e50ef10f5c578c8e0eb1a1a171e2
Jhonny Lopes <jhonny.lopes@discover.com.br>
2021-05-31 22:30:05 -0300

    bootstrap jquery e wow nao ficam bem

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 // JS Goes here - ES6 supported
 
+import "./bootstrap.min.js";
+import "./venobox.min.js";
+import "./wow.min.js";
+import "./slick.min.js";
+import "./principal.js";
+
+import "./css/animate.min.css";
+import "./css/bootstrap.min.css";
+import "./css/font-awesome.min.css";
 import "./css/main.css";
+import "./css/responsive.css";
+import "./css/slick.css";
+import "./css/venobox.css";
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
--- webpack.common.js
+++ webpack.common.js
@@ -55,5 +55,11 @@ module.exports = {
         flatten: true
       }
     ])
-  ]
+  ],
+
+  externals: {
+    // require("jquery") is external and available
+    //  on the global var jQuery
+    "jquery": "jQuery"
+  }
 };
